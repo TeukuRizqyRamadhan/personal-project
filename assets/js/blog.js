4e7e2d86f6ead49fa57261ebcd0f6e12eae96ccb
TeukuRizqyRamadhan <kikidotid@gmail.com>
2024-02-21 09:18:10 +0700

    push to lastest coding to main

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,27 +1,26 @@
-let dataBlogs = [];
+ let dataBlogs = [];
 
 function addBlog(event) {
   event.preventDefault();
 
   let title = document.getElementById("input-blog-title").value;
-  let datestart = document.getElementById("input-blog-start").value;
-  let dateend = document.getElementById("input-blog-end").value;
   let desc = document.getElementById("input-blog-desc").value;
   let technologyInput = [...document.querySelectorAll("input[name='technology']:checked")];
   
   let technology = technologyInput.map(item=>item.value)
+  
+  let durationTime = getDurationTime();
 
   let dataBlog = {
     title,
-    datestart,
-    dateend,
     desc,
-    technology
+    technology,
+    durationTime
   };
   
   dataBlogs.push(dataBlog);
 
-  console.log(dataBlogs);
+  // console.log(dataBlogs);
 
   renderBlog();
 
@@ -31,17 +30,17 @@ function renderBlog() {
   document.getElementById("contents").innerHTML = "";
 
   for (let index = 0; index < dataBlogs.length; index++) {
+
     document.getElementById("contents").innerHTML += `
     <div class="container">
     <div class="card">
         <img src="assets/img/blog-img.png">
         <div class="blog-content">
         <h1 id"title-h1">
-        <a href="projectDetail.html" target="_blank"
-        >${dataBlogs[index].title}</a>
+        <a href="projectDetail.html?id=${index}" target="_blank return false;">${dataBlogs[index].title}</a>
         </h1>
         <div class="detail-blog-content">
-        <p>durasi : 3 bulan</p>
+        <p>durasi : ${dataBlogs[index].durationTime}</p>
         </div>
         <p>
         ${dataBlogs[index].desc}
@@ -50,8 +49,8 @@ function renderBlog() {
         ${dataBlogs[index].technology.map((item)=>`<i class='${item}'></i>`).join(" ")}
         </div>
         <div class="btn-group">
-          <button class="button">Edit Post</button>
-          <button class="btn-post">Delete Post</button>
+          <button class="button">Edit</button>
+          <button class="btn-post">Delete</button>
         </div>
     </div>
     </div>
@@ -59,41 +58,32 @@ function renderBlog() {
     `;
   }
 }
-// function renderDetail(id) {
-// }
 
-// const sayHello = (data) => {
-//   return `<i class='${data}'></i>`
-// }
+function getDurationTime(){
 
-// (item) => {
-//   return `<i class='${item}'></i>`
-// }
+  let dateStart = document.getElementById("input-blog-start").value;
+  let dateEnd = document.getElementById("input-blog-end").value;
+  let inputStartDate = new Date(dateStart);
+  let inputEndDate = new Date(dateEnd);
 
-// (data) => `<i class='${data}'></i>`
+  let durationTime = inputEndDate - inputStartDate;
 
-// data => `<i class='${data}'></i>`
+  let milisecond = 1000 // milisecond
+  let secondInHour = 3600 // 1 jam = 3600 detik
+  let hourInDay = 24 // 1 hari - 24 jam
+  let dayInMonth = 30 // 30 hari dalam 1 bulan
 
+  let durationMonth = Math.floor(
+    durationTime / (milisecond * secondInHour * hourInDay * dayInMonth)
+  );
+  
+  let durationDay = Math.floor(
+    durationTime / (milisecond * secondInHour * hourInDay)
+  );
 
-// function (item) {
-//   return `<i class='${item}'></i>`
-// }
-
-// let technology = []
-
-// let technologyInput = [...document.querySelectorAll("input[name='technology']:checked")];
-// let initechnology2 = ["fa-brands fa-node fa-2xl", "fa-brands fa-golang fa-2xl"]
-
-// let technologyInput2 = [document.querySelectorAll("input[name='technology']:checked")];
-// let technology2 = ["fa-brands fa-golang fa-2xl"]
-
-// = : 
-// let myName = "Teuku Rizqy Ramadhan"
-
-// myName = "Samsul"
-
-// ==
-// 0 == "0" : true 
-
-
-// ===z
+    if (durationMonth > 0){
+    return `${durationMonth} bulan`;
+  } else if(durationDay > 0) {
+    return `${durationDay} hari`;
+  }
+}
