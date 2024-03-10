1ccda68cc569d7d9e2455d3618f4c1d378f0922f
TeukuRizqyRamadhan <kikidotid@gmail.com>
2024-03-10 16:27:42 +0700

    add session

--- src/database/seeders/20240310091635-demo-users.js
+++ src/database/seeders/20240310091635-demo-users.js
@@ -4,17 +4,10 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
 
-    await queryInterface.bulkInsert('projects', [{
-      title: "dumbways",
-      start_date: new Date("2024-01-22"),
-      end_date: new Date("2024-01-25"),
-      image: "https://github.com/images/modules/search/dark.png",
-      content: "cihuy",
-      node: true,
-      react: true,
-      golang: true,
-      js: false,
-      diff_date: '10 hari',
+    await queryInterface.bulkInsert('users', [{
+      name: 'Teuku Rizqy Ramadhan',
+      email: ' teukurizqyr@gmail.com',
+      password: '123123',
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
